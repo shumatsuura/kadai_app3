12a81c4f10c0d5b18aacbd2d16c3684b4c1a58d1
shumatsuura <shunsuke.mtr@gmail.com>
2020-01-18 22:47:06 +0900

    サインイン、ログイン、ユーザー編集（画像除く）実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id), notice: "Sign Upしました！"
+    else
+      render'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: "編集しました。"
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email,:profile_photo,:image_cache, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :users,only:[:new,:create,:show,:edit,:update]
+  resources :sessions,only:[:new,:create,:destroy]
+  resources :feeds
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
