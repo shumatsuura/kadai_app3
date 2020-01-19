c5f270ebe58009edb725c1ed17633c786b776ada
shumatsuura <shunsuke.mtr@gmail.com>
2020-01-19 21:19:17 +0900

    feedモデル、feedsコントローラー等削除

--- app/models/feed.rb
+++
@@ -1,8 +0,0 @@
-class Feed < ApplicationRecord
-  belongs_to :user
-  has_many :favorites, dependent: :destroy
-  has_many :favorite_users, through: :favorites, source: :user
-  mount_uploader :image, ImageUploader
-  validates :content, presence:true
-  validates :image, presence:true
-end
